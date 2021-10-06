ce19edff0971cce5a6604d21170fcc1c4fcba047
Charlie Nader <nader@Charlies-MacBook-Pro-2.local>
2021-10-06 19:56:44 +1100

    adding moran middleware for log, adding launche model

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 const app = express();
 
@@ -12,13 +14,15 @@ app.use(
   })
 );
 
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
