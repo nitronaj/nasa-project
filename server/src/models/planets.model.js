a08456d39b6a075034ec4446747cca9754a85f57
Charlie Nader <nitro.naj@gmail.com>
2021-10-21 21:03:39 +1100

    feat(mongoose) adding schema, model for launche and planet

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const parse = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -24,9 +26,13 @@ function loadPlanetsData() {
           columns: true,
         })
       )
-      .on('data', (data) => {
+      .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          // habitablePlanets.push(data);
+          // TODO: Replace below create with insert + update = upsert
+          // await planets.create({
+          //   keplerName: data.kepler_name,
+          // });
         }
       })
       .on('error', (err) => {
@@ -42,8 +48,8 @@ function loadPlanetsData() {
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({});
 }
 
 module.exports = {
--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,32 @@
-const { stat } = require("fs");
-const http = require("http");
-const app = require("./app");
+const http = require('http');
+const mongoose = require('mongoose');
 
-const { loadPlanetsData } = require("./models/planets.model");
+const app = require('./app');
+
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  'mongodb+srv://nasa-api:FQP9NnBO51WFTK53@nasacluster.5dwnc.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
-async function startServer() {
-	await loadPlanetsData();
+mongoose.connection.once('open', () => {
+  console.log('MongoDb connection read!');
+});
 
-	server.listen(PORT, () => {
-		console.log(`Server listening on port ${PORT}`);
-	});
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 }
 
 startServer();
