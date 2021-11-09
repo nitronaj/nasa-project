f5aa818c8b497d9fd35e68b9d6b3a7565a8bd5f4
Charlie Nader <nitro.naj@gmail.com>
2021-11-09 20:18:29 +1100

    add versioning to the API

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const mongoConnect = require('./services/mongo');
+const { mongoConnect } = require('./services/mongo');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 async function startServer() {
-  mongoConnect();
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
