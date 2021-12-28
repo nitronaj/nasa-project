87f18516a8342211d887bf952c12104aaeccac59
Charlie Nader <nitro.naj@gmail.com>
2021-12-28 17:40:52 +1100

    add dotenv config

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://nasa-api:FQP9NnBO51WFTK53@nasacluster.5dwnc.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDb connection ready!');
